26b6a3eae2072a9384c24c45b897d6a10f1dd5ab
Harsh <harshk1729@gmail.com>
2025-01-08 01:49:51 +0530

    Added routes logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,62 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import {decode, verify, sign} from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL: string
+    JWT_seceret: string
+  }
+}>()
 
-app.post('/api/v1/signup',(c)=>{
-  return c.text('hello')
+// const dburl = process.env.DATABASE_URL can't use here.
+
+app.post('/api/v1/signup',async (c)=>{
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json()
+
+  const check = await prisma.user.findUnique({
+    where:{
+      email: body.email
+    }
+  })
+  if(!check){
+    c.status(403);
+    return c.json({error: 'User already exist'})
+  }
+
+  const user = await prisma.user.create({
+    data:{
+      email: body.email,
+      password: body.password,
+      name: body.name
+    }
+  })
+  const token = sign({id: user.id}, c.env.JWT_seceret)
+  return c.json({token: token})
 })
 
-app.post('/api/v1/signin',(c)=>{
-  return c.text('hello')
+app.post('/api/v1/signin',async (c)=>{
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json()
+
+  const check = await prisma.user.findUnique({
+    where:{
+      email: body.email
+    }
+  })
+  if(!check){
+    c.status(403)
+    return c.json({error: 'User already exist'})
+  }
+  const token = sign({id: check.id}, c.env.JWT_seceret)
+  return c.json({token})
 })
 
 app.post('/api/v1/blog',(c)=>{
