d20378c6f13472899d1f3dd956819e0aeca8b1fa
Harsh <harshk1729@gmail.com>
2025-01-07 23:08:41 +0530

    First commit

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post('/api/v1/signup',(c)=>{
+  return c.text('hello')
+})
+
+app.post('/api/v1/signin',(c)=>{
+  return c.text('hello')
+})
+
+app.post('/api/v1/blog',(c)=>{
+  return c.text('hello')
+})
+
+app.put('/api/v1/blog',(c)=>{
+  return c.text('hello')
+})
+
+app.get('/api/v1/blog:id',(c)=>{
+  return c.text('hello')
+})
+
+export default app
