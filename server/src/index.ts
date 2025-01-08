4d766a59a096adc36ba9b63a07fbbf5398c5c42c
Harsh <harshk1729@gmail.com>
2025-01-08 17:37:35 +0530

    Updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,73 +1,102 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import {decode, verify, sign} from 'hono/jwt'
+import bcrypt from 'bcryptjs'
+
+type Binding = {
+  DATABASE_URL: string,
+  JWT_Secret: string
+}
 
 const app = new Hono<{
-  Bindings:{
-    DATABASE_URL: string
-    JWT_seceret: string
-  }
+  Bindings:Binding
 }>()
 
 // const dburl = process.env.DATABASE_URL can't use here.
+
+app.use('/api/v1/blog/*',async (c,next)=>{
+  const header = c.req.header("authorization") || ""
+  const token = header.split(" ")[1]
+  const verification = await verify(token, c.env.JWT_Secret)
+  if(verification.id){
+    await next()
+  }
+  else{
+    return c.json({msg:"Unauthorized"})
+  }
+})
 
 app.post('/api/v1/signup',async (c)=>{
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
-
-  const body = await c.req.json()
-
-  const check = await prisma.user.findUnique({
-    where:{
-      email: body.email
-    }
-  })
-  if(!check){
-    c.status(403);
-    return c.json({error: 'User already exist'})
-  }
-
-  const user = await prisma.user.create({
-    data:{
-      email: body.email,
-      password: body.password,
-      name: body.name
-    }
-  })
-  const token = sign({id: user.id}, c.env.JWT_seceret)
-  return c.json({token: token})
+        try{const body = await c.req.json()
+        
+          const check = await prisma.user.findUnique({
+            where:{
+              email: body.email
+            }
+          })
+          if(check){
+            c.status(403);
+            return c.json({error: 'User already exist'})
+          }
+          const hashing = await bcrypt.hash(body.password, 10)
+        
+          const user = await prisma.user.create({
+            data:{
+              email: body.email,
+              password: hashing,
+              name: body.name
+            }
+          })
+          const token = await sign({id: user.id}, c.env.JWT_Secret)
+          return c.json({msg:'User created successfully', token})}
+    
+    catch(error){
+      return c.json({msg: 'Error in signup. Try again'})
+    } 
+  
 })
 
 app.post('/api/v1/signin',async (c)=>{
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-  const body = await c.req.json()
+      try{const body = await c.req.json()
 
-  const check = await prisma.user.findUnique({
-    where:{
-      email: body.email
+        const check = await prisma.user.findUnique({
+          where:{
+            email: body.email,
+          }
+        })
+        if(!check){
+          c.status(403)
+          return c.json({error: 'User does not exist'})
+        }
+        const isValid = await bcrypt.compare(body.password, check.password);
+        if(!isValid){
+          return c.json({msg:"Incorrect password"})
+        }
+        const token = await sign({id: check.id}, c.env.JWT_Secret)
+        return c.json({token, msg:'Successfully Logged In'})}
+    
+    catch{
+      return c.json({msg:'Error in signin try again'})
     }
-  })
-  if(!check){
-    c.status(403)
-    return c.json({error: 'User already exist'})
-  }
-  const token = sign({id: check.id}, c.env.JWT_seceret)
-  return c.json({token})
+  
 })
 
-app.post('/api/v1/blog',(c)=>{
+app.post('/api/v1/blog/post',(c)=>{
   return c.text('hello')
 })
 
-app.put('/api/v1/blog',(c)=>{
+app.put('/api/v1/blog/update',(c)=>{
   return c.text('hello')
 })
 
-app.get('/api/v1/blog:id',(c)=>{
+app.get('/api/v1/blogg',(c)=>{
   return c.text('hello')
 })
 
